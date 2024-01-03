bc35fb2c7a7e3da337b8534284178f048d5004c5
Lakeshwar <110439492+lakeshwarmandal@users.noreply.github.com>
2024-01-03 13:32:34 -0600

    Somthing Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-// src/App.js
 import React from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeForm from "./components/RecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
       <nav>
-        <ul>
-          <li>
+        <ul className="test">
+          <li className="test1">
             <Link to="/recipe-list">Recipe List</Link>
           </li>
-          <li>
+          <li className="test2">
             <Link to="/add-recipe">Add Recipe</Link>
           </li>
         </ul>
       </nav>
       <hr />
-      <Routes>
-        <Route path="/recipe-list" element={<RecipeList />} />
-        <Route path="/add-recipe" element={<RecipeForm />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/recipe-list" element={<RecipeList />} />
+          <Route path="/add-recipe" element={<RecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
     </div>
   );
 };
